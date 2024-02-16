c0b218dd8885aa772b44f530f07fab3303ea04b9
Krohn <krohnkg@gmail.com>
2024-02-16 11:00:36 +0100

    New param

--- deffinition.py
+++ deffinition.py
@@ -1,21 +1,22 @@
 import numpy as np
 import random
 
+x_wall_price_sqr_cm = 0.6
+y_wall_price_sqr_cm = 0.8
+flat_price_sqr_cm = 0.1
 
-flat_price_sqr_cm = 0.8
-y_wall_price_sqr_cm = 0.5
-x_wall_price_sqr_cm = 0.2
-mutation_rate = 0.01
+
+mutation_rate = 0.002
 var_min = 1
-var_max = 5
+var_max = 1000
 
 
-def create_genes():
-    number = random.randint(1, 3)
+def create_genes():#make the gene
+    number = random.randint(1, 1000)
     return number
 
 
-def create_crom():
+def create_crom(): # create the chromosome
     x = create_genes()
     y = create_genes()
     z = create_genes()
@@ -23,43 +24,43 @@ def create_crom():
     return vec
 
 
-def cal_fitness(box_dim):
+def cal_fitness(box_dim): # calculate the price of the walls of the box
     fitness = ((box_dim[0] * box_dim[1] * flat_price_sqr_cm) * 2  # x * y * roof/floor
                + (box_dim[0] * box_dim[2] * x_wall_price_sqr_cm) * 2  # x * z * xWall
                + (box_dim[2] * box_dim[2] * y_wall_price_sqr_cm) * 2)  # y * z * yWall
     return fitness
 
 
-def cal_volume(box_dim):
+def cal_volume(box_dim): # calculate the volume
     fitness = box_dim[0] * box_dim[1] * box_dim[2]
     return fitness
 
 
-def cal_volume_fitness(box_dim):
+def cal_volume_fitness(box_dim):# calculate the Price pr volume fitness
     fitness = cal_fitness(box_dim)/(cal_volume(box_dim))#fitness/vl\olum?? isteden
     return fitness
 
 
-def crossover(parent1, parent2):
+def crossover(parent1, parent2): # Uniform crossover implementation
     crossover_point = random.randint(0, len(parent1) - 1)
     child1 = np.concatenate((parent1[:crossover_point], parent2[crossover_point:]))
     child2 = np.concatenate((parent2[:crossover_point], parent1[crossover_point:]))
     return child1, child2
 
 
-def mutation(child):
+def mutation(child): # add the mutation to the variable
     mutated_child = child.copy()
     for i in range(len(mutated_child)):
         if random.random() < mutation_rate:
-            mutated_child[i] += random.randint(var_min, var_max)
+            mutated_child[i] = random.randint(var_min, var_max)
     return mutated_child
 
 
-def create_offspring(population):
+def create_offspring(population): # Make the new population.
     new_generation = []
     while len(new_generation) < len(population):
-        parent1 = random.choice(population[:3])
-        parent2 = random.choice(population[:4])
+        parent1 = random.choice(population[:10])
+        parent2 = random.choice(population[:10])
         child1, child2 = crossover(parent1, parent2)
         child1 = mutation(child1)
         child2 = mutation(child2)
@@ -68,6 +69,6 @@ def create_offspring(population):
     return new_generation
 
 
-def sort_strings_by_fitness(list_of_lists):
+def sort_strings_by_fitness(list_of_lists): #sort the list based on the best price to volume metric.
     return sorted(list_of_lists, key=lambda x: cal_volume_fitness(x))
 
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import deffinition
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 
-num_pop = 500
-num_genes = 100
-
+num_pop = 100
+num_genes = 50
+Best_cost_val = float('inf')
+Best_cost = []
 plot_fitness = []
 plot_gen = []
-
 generation = 1
 population = []
 found = False
@@ -18,33 +17,35 @@ for x in range(num_pop):  # initialize first population
 
 while not found:
 
-    # sort list with lowest finest first
-    population = deffinition.sort_strings_by_fitness(population)
+    population = deffinition.sort_strings_by_fitness(population)  # sort list with lowest finest first
+    if Best_cost_val > deffinition.cal_volume_fitness(population[0]):
+        Best_cost_val = deffinition.cal_volume_fitness(population[0])
+
+    Best_cost.append(Best_cost_val)
 
     if generation == num_genes:  # break the while loop condition
         found = True
         break
 
-    new_generation = deffinition.create_offspring(population)
+    new_generation = deffinition.create_offspring(population)  # generate the new population
 
-    population = new_generation
-
-    plot_fitness.append(deffinition.cal_volume_fitness(population[0]))
-    plot_gen.append(generation)
+    population = new_generation  # save the new population for next generation.
 
     generation = generation + 1
+    plot_fitness.append(deffinition.cal_volume_fitness(population[0]))  # save the plot data
+    plot_gen.append(generation)  # save the plot data
 
+# Plot the findings
 fig, ax = plt.subplots()
-ax.plot(plot_gen, plot_fitness)
-# Change x-axis and y-axis tick spacing
-ax.xaxis.set_major_locator(ticker.MultipleLocator(base=5))  # x-axis ticks at multiples of 2
-ax.yaxis.set_major_locator(ticker.MultipleLocator(base=0.1))  # y-axis ticks at multiples of 0.5
+ax.plot(Best_cost)
+
 plt.xlabel("Generations")
 plt.ylabel("Price pr m^3")
-
 plt.grid()
 plt.show()
-print("Box dimensjons x y z: ", population[0])
+# Print the results
+print("Box dimensjons x y z: ", population[0])  # in cm
 print("Gen: ", generation)
 print("BOX volume m^3: ", deffinition.cal_volume(population[0]) / 1000000)  # cm^3 to m^3
-print("BOX price: ", deffinition.cal_fitness(population[0]) / 10000)
+print("BOX price: ", deffinition.cal_fitness(population[0]) / 10000)  # Price
+print("Price pr volum: ", deffinition.cal_volume_fitness(population[0]))  # Price
